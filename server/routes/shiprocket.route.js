e0220c6829a462015222c0d3c019225770eade93
yogeshwar verma <yogeshwarverma08@gmail.com>
2026-08-25 15:32:58 +0530

    More flexible and detailed Shiprocket result

--- server/routes/shiprocket.route.js
+++ server/routes/shiprocket.route.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const { shipOrder, trackOrder } = require("../controllers/shiprocket.controller");
+const { shipOrder, retryAssignAWB, trackOrder } = require("../controllers/shiprocket.controller");
 const { protect, adminOnly } = require("../middleware/auth.middleware");
 
 router.use(protect, adminOnly);
 
-router.post("/ship/:orderId",   shipOrder);
-router.get("/track/:orderId",   trackOrder);
-router.post("/retry-awb/:orderId",    retryAssignAWB);
+router.post("/ship/:orderId",      shipOrder);
+router.post("/retry-awb/:orderId", retryAssignAWB);
+router.get("/track/:orderId",      trackOrder);
 
 module.exports = router;
